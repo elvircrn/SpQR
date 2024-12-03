95df72ee31f47e38659542ceb91965d48b8d6912
Elvir Crncevic <elvircrn@gmail.com>
2024-12-03 17:09:48 +0100

    entroy_analys.py

--- entropy_analysis.py
+++ entropy_analysis.py
@@ -8,6 +8,7 @@ if __name__ == '__main__':
         for t in os.listdir(os.path.join(base_path, p)):
             if os.path.isfile(os.path.join(base_path, p, t)):
                 t = torch.load(os.path.join(base_path, p, t))
+                print(t.keys())
                 W = t['quant_weights']
                 m = W.shape[0]
                 n = W.shape[1]
