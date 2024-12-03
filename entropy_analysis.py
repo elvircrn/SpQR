62c3a141d5b8cee7f14d45647ecb6f937a711e51
Elvir Crncevic <elvircrn@gmail.com>
2024-12-03 17:08:02 +0100

    entroy_analys.py

--- entropy_analysis.py
+++ entropy_analysis.py
@@ -6,12 +6,13 @@ if __name__ == '__main__':
     base_path = sys.argv[1]
     for p in os.listdir(base_path):
         for t in os.listdir(os.path.join(base_path, p)):
-            t = torch.load(os.path.join(base_path, p, t))
-            W = t['quant_weights']
-            m = W.shape[0]
-            n = W.shape[1]
-            values, counts = torch.unique(W, return_counts=True)
-            nnz = t["outliers_matrix"].to_sparse_csr().values().shape[0]
-            counts = counts.float() / counts.float().sum()
-            print(f'Tensor {t} stats\nnnz = {nnz}\n:counts = {counts}\nmean = {torch.mean(counts)}\nvariance = {torch.var(counts)}')
-            break
+            if os.path.isfile(t):
+                t = torch.load(os.path.join(base_path, p, t))
+                W = t['quant_weights']
+                m = W.shape[0]
+                n = W.shape[1]
+                values, counts = torch.unique(W, return_counts=True)
+                nnz = t["outliers_matrix"].to_sparse_csr().values().shape[0]
+                counts = counts.float() / counts.float().sum()
+                print(f'Tensor {t} stats\nnnz = {nnz}\n:counts = {counts}\nmean = {torch.mean(counts)}\nvariance = {torch.var(counts)}')
+                break
