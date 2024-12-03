449f2f319f111b979231e89278af1c9fb7c8f661
Elvir Crncevic <elvircrn@gmail.com>
2024-12-03 17:16:48 +0100

    remove ispath check

--- entropy_analysis.py
+++ entropy_analysis.py
@@ -6,15 +6,16 @@ if __name__ == '__main__':
     base_path = sys.argv[1]
     for p in os.listdir(base_path):
         for t in os.listdir(os.path.join(base_path, p, '0')):
-            tensor_path = os.path.join(base_path, p, t)
+            tensor_path = os.path.join(base_path, p, t, '0')
             print(f'tensor_path = {tensor_path}')
-            t = torch.load(tensor_path, map_location='cpu')
-            print(t.keys())
-            W = t['quant_weights']
-            m = W.shape[0]
-            n = W.shape[1]
-            values, counts = torch.unique(W, return_counts=True)
-            nnz = t["outliers_matrix"].to_sparse_csr().values().shape[0]
-            counts = counts.float() / counts.float().sum()
-            print(f'Tensor {t} stats\nnnz = {nnz}\n:counts = {counts}\nmean = {torch.mean(counts)}\nvariance = {torch.var(counts)}')
-            break
+            if os.path.isfile(tensor_path):
+                t = torch.load(tensor_path, map_location='cpu')
+                print(t.keys())
+                W = t['quant_weights']
+                m = W.shape[0]
+                n = W.shape[1]
+                values, counts = torch.unique(W, return_counts=True)
+                nnz = t["outliers_matrix"].to_sparse_csr().values().shape[0]
+                counts = counts.float() / counts.float().sum()
+                print(f'Tensor {t} stats\nnnz = {nnz}\n:counts = {counts}\nmean = {torch.mean(counts)}\nvariance = {torch.var(counts)}')
+                break
