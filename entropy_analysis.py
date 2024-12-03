c865d4fe769d01c6a4d402e06bc7c1fc1cbf431c
Elvir Crncevic <elvircrn@gmail.com>
2024-12-03 17:19:49 +0100

    remove ispath check

--- entropy_analysis.py
+++ entropy_analysis.py
@@ -5,8 +5,8 @@ import sys
 if __name__ == '__main__':
     base_path = sys.argv[1]
     for p in os.listdir(base_path):
-        for t in os.listdir(os.path.join(base_path, p, '0')):
-            tensor_path = os.path.join(base_path, p, '0', t)
+        for t_name in os.listdir(os.path.join(base_path, p, '0')):
+            tensor_path = os.path.join(base_path, p, '0', t_name)
             print(f'tensor_path = {tensor_path}')
             if os.path.isfile(tensor_path):
                 t = torch.load(tensor_path, map_location='cpu')
@@ -16,5 +16,4 @@ if __name__ == '__main__':
                 values, counts = torch.unique(W, return_counts=True)
                 nnz = t["outliers_matrix"].to_sparse_csr().values().shape[0]
                 counts = counts.float() / counts.float().sum()
-                print(f'Tensor {t} stats\nnnz = {nnz}\n:counts = {counts}\nmean = {torch.mean(counts)}\nvariance = {torch.var(counts)}')
-                break
+                print(f'Tensor {t_name} stats\nnnz = {nnz}\n:counts = {counts}\nmean = {torch.mean(counts)}\nvariance = {torch.var(counts)}')
