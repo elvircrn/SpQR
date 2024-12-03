b72586269899e778b388a83fdc63637c5180df54
Elvir Crncevic <elvircrn@gmail.com>
2024-12-03 17:26:06 +0100

    remove ispath check

--- entropy_analysis.py
+++ entropy_analysis.py
@@ -4,10 +4,10 @@ import sys
 
 if __name__ == '__main__':
     base_path = sys.argv[1]
-    print('tensor;nnz;sparsity;mean;variance')
-    for p in os.listdir(base_path):
-        for t_name in os.listdir(os.path.join(base_path, p, '0')):
-            tensor_path = os.path.join(base_path, p, '0', t_name)
+    print('tensor;nnz;sparsity;mean;variance;outlier_threshold')
+    for outlier_threshold in os.listdir(base_path):
+        for t_name in os.listdir(os.path.join(base_path, outlier_threshold, '0')):
+            tensor_path = os.path.join(base_path, outlier_threshold, '0', t_name)
             # print(f'tensor_path = {tensor_path}')
             if os.path.isfile(tensor_path):
                 t = torch.load(tensor_path, map_location='cpu')
@@ -19,4 +19,4 @@ if __name__ == '__main__':
                 counts = counts.float() / counts.float().sum()
                 # print(f'Tensor {t_name} stats\nnnz = {nnz}\n:counts = {counts}\nmean = {torch.mean(counts)}\nvariance = {torch.var(counts)}')
                 if 'q_proj' in tensor_path:
-                    print(f'{os.path.basename(tensor_path)};{nnz};{1 - nnz / (m * n)};{torch.mean(counts)};{torch.var(counts)}')
+                    print(f'{os.path.basename(tensor_path)};{nnz};{1 - nnz / (m * n)};{torch.mean(counts):.4f};{torch.var(counts):.4f};{outlier_threshold}')
